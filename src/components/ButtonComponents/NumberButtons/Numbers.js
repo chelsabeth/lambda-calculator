3c369be882bc4f7ff273b51cba1d37049403b606
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-09-10 16:00:27 -0400

    export and imports, mapping through numbers

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -15,7 +15,10 @@ const Numbers = () => {
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component*/
+       numbers.map(numberState, setNumberState) => (
+         
+       )}
     </div>
   );
 };
