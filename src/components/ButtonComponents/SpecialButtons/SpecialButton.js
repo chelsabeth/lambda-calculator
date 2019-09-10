4958e9104dc2e2d16a6f15291f76ffb9b35017aa
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-09-10 16:42:34 -0400

    mapped through all arrays

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { specials } from "../../../data";
+
 
 const SpecialButton = () => {
   return (
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { specials } from "../../../data";
 
 //import any components needed
 
@@ -11,7 +12,10 @@ const Specials = () => {
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component*/
+       specials.map((item, index) => (
+         <OperatorButton key={index} item={item} />
+       ))}
     </div>
   );
 };
