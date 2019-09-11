cc7ef68e355d6f3c673dd6d192b1fc4051865462
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-09-10 21:02:31 -0400

    specials work

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <Numbers />
         <Operators />
+        <Specials />
       </div>
     </div>
   );
