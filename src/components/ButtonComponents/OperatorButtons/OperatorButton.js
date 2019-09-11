efc52532f2e6ad9ab4b0317a972acbc80c2c0761
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-09-10 20:59:57 -0400

    objects work

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,12 +1,11 @@
 import React from "react";
 
-
 const OperatorButton = props => {
   console.log('props', props);
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button>{props.item}</button>
+      <button>{props.item.char}</button>
     </>
   );
 };
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { operators } from "../../../data";
 import OperatorButton from "./OperatorButton";
 
 //import any components needed
+
+import { operators } from "../../../data";
 
 //Import your array data to from the provided data file
 
