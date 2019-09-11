151b9e502144fa4357758f30a4507532f86a466c
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-09-11 17:52:59 -0400

    display looking good. MVP

--- src/App.js
+++ src/App.js
@@ -23,10 +23,10 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Display NumberValue={screen}/>
         <Numbers />
         <Operators />
         <Specials />
-        <Display NumberValue={screen}/>
       </div>
     </div>
   );
--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -2,7 +2,6 @@ import React from "react";
 import { tsPropertySignature } from "@babel/types";
 
 const Display = props => {
-  const screen='0'
   return (
   <div className="display">{props.NumberValue}</div>
   );
